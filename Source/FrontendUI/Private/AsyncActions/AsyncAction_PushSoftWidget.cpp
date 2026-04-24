17592822bb0e571f5d4fb67408e950b3815368f1
DanielSasaki <daniel.sasaki87@gmail.com>
2026-04-24 19:53:54 -0300

    Adding AsyncAction_PushSoftWidget class

--- Source/FrontendUI/Private/AsyncActions/AsyncAction_PushSoftWidget.cpp
+++ Source/FrontendUI/Private/AsyncActions/AsyncAction_PushSoftWidget.cpp
@@ -2,6 +2,8 @@
 
 
 #include "AsyncActions/AsyncAction_PushSoftWidget.h"
+#include "Subsystems/FrontendUISubsystem.h"
+#include "Widgets/Widget_ACtivatableBase.h"
 
 UAsyncAction_PushSoftWidget* UAsyncAction_PushSoftWidget::PushSoftWidget(
 	const UObject* WorldContextObject,
@@ -17,6 +19,11 @@ UAsyncAction_PushSoftWidget* UAsyncAction_PushSoftWidget::PushSoftWidget(
 		if (UWorld* World = GEngine->GetWorldFromContextObject(WorldContextObject, EGetWorldErrorMode::LogAndReturnNull))
 		{
 			UAsyncAction_PushSoftWidget* Node = NewObject<UAsyncAction_PushSoftWidget>();
+			Node->CachedOwningWorld = World;
+			Node->CachedOwningPC = OwningPlayerController;
+			Node->CachedSoftWidgetClass = InSoftWidgetClass;
+			Node->CachedWidgetStackTag = InWidgetStackTag;
+			Node->bCachedFocusOnNewlyPushedWidget = bFocusOnNewlyPushedWidget;
 			
 			Node->RegisterWithGameInstance(World);
 			
@@ -25,4 +32,44 @@ UAsyncAction_PushSoftWidget* UAsyncAction_PushSoftWidget::PushSoftWidget(
 	}
 	
 	return nullptr;
+}
+
+void UAsyncAction_PushSoftWidget::Activate()
+{
+	UFrontendUISubsystem* UFrontendUISubsystem = UFrontendUISubsystem::Get(CachedOwningWorld.Get());
+	
+	UFrontendUISubsystem->PushSoftWidgetToStackAsync(CachedWidgetStackTag, CachedSoftWidgetClass,
+		[this](EAsyncPushWidgetState InPushState, UWidget_ActivatableBase* PushedWidget)
+		{
+			switch (InPushState)
+			{
+				case EAsyncPushWidgetState::OnCreatedBeforePush:
+				
+					PushedWidget->SetOwningPlayer(CachedOwningPC.Get());
+				
+					OnWidgetCreatedBeforePush.Broadcast(PushedWidget);
+				
+					break;
+				
+				case EAsyncPushWidgetState::AfterPush:
+				
+					AfterPush.Broadcast(PushedWidget);
+				
+					if (bCachedFocusOnNewlyPushedWidget)
+					{
+						if (UWidget* WidgetToFocus = PushedWidget->GetDesiredFocusTarget())
+						{
+							WidgetToFocus->SetFocus();
+						}
+					}
+				
+					SetReadyToDestroy();
+				
+					break;
+				
+				default: 
+					break;
+			}
+		}	
+	);
 }
